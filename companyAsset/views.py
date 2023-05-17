777d5ad252c0c42470abe875234358cdf68abfc1
mossaddak <mossaddak15-2413@diu.edu.bd>
2023-05-18 03:13:44 +0600

    updated

--- companyAsset/views.py
+++ companyAsset/views.py
@@ -4,21 +4,34 @@ from .models import(
 )
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from userProfileApp.models import(
+    User
+)
 
 
 # Create your views here.
 def AddAsset(request):
+    user = User.objects.get(username=request.user.username)
+    company_assets = user.CompanyAssetUserRelatedname.all()
+    print("AllAsset======================================>", company_assets)
     name = request.GET.get("asset_name")
+    is_asset_present = company_assets.filter(name=name)
+
     if request.user.is_authenticated:
-        if request.method=="GET" and request.user.user_type == "Company":
-            blog1 = CompanyAsset.objects.create(
-                user=request.user,
-                name=name
-            )
-            blog1.save()
-            messages.success(request,f"{name} successfully added.")
+
+        if not is_asset_present:
+
+            if request.method=="GET" and request.user.user_type == "Company":
+                blog1 = CompanyAsset.objects.create(
+                    user=request.user,
+                    name=name
+                )
+                blog1.save()
+                messages.success(request,f"{name} successfully added.")
+            else:
+                messages.warning(request,f"You don't have permission for this action")
         else:
-            messages.warning(request,f"You don't have permission for this action")
+            messages.warning(request,f"{name} Allready present")
     else:
         messages.warning(request,f"please log into your account.")
 
